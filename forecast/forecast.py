e723d5422089c10cf62e1f65060153a69b1b3b72
patogallardo <26889221+patogallardo@users.noreply.github.com>
2024-05-10 12:14:36 -0500

    updated forecast with desi and SO numbers

--- forecast/forecast.py
+++ forecast/forecast.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 SNR_CURRENT = 3.1
-NOISE_ukarcmin_CURRENT = 30
-NGAL_CURRENT = 300000
+NOISE_ukarcmin_CURRENT = 14
+NGAL_CURRENT =  227837
 
 def forecast_SNR(snr_current, noise_current, ngal_current, noise_forecast, ngal_forecast):
     return snr_current * noise_current/noise_forecast * np.sqrt(ngal_forecast/ngal_current)
 
-Ngal_forecast = 1000000
-noise_ukarcmin_forecast = 10
+Ngal_forecast = 4000000
+noise_ukarcmin_forecast = 6
 
 forecasted = forecast_SNR(SNR_CURRENT, NOISE_ukarcmin_CURRENT, NGAL_CURRENT,
                           noise_ukarcmin_forecast, Ngal_forecast)
@@ -17,5 +17,6 @@ forecasted = forecast_SNR(SNR_CURRENT, NOISE_ukarcmin_CURRENT, NGAL_CURRENT,
 print("\\newcommand{\\CURRENTNOISE}{%i}" % NOISE_ukarcmin_CURRENT)
 print("\\newcommand{\\CURRENTNGAL}{%s}" % f'{NGAL_CURRENT:,}')
 print("\\newcommand{\\NOISEFORECAST}{%i}" % noise_ukarcmin_forecast)
-print("\\newcommand{\\NGALFORECAST}{%s}" % f'{Ngal_forecast:,}')
+Ngal_forecast_to_print_million = int(Ngal_forecast/1e6)
+print("\\newcommand{\\NGALFORECASTMILLIONS}{%s}" % f'{Ngal_forecast_to_print_million:,d}')
 print("\\newcommand{\\SNRFORECASTEDSO}{%i}" % forecasted)
